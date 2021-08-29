b570ac46bc8b6d4e6f04fc1ebb11db9edb98a0a3
last-yousof <yousof.fetyani@gmail.com>
2021-08-29 21:10:10 +0300

    Update bikeshare.py
    Updated doc string in time_stats function.

--- bikeshare.py
+++ bikeshare.py
@@ -88,7 +88,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+    """Displays statistics on the most frequent times of travel in the cities"""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
